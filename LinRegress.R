c02943cd4ac782ae975676768b5f24b47427a7b4
Megan Tran <68253811+Sonicdaheghod@users.noreply.github.com>
2023-08-24 20:57:33 -0400

    Add files via upload

--- LinRegress.R
+++ LinRegress.R
@@ -1,3 +1,6 @@
+#Code source reference: https://www.youtube.com/watch?v=5bA0m1OowdU&list=PLXCw5VdOQb7iN847MZzb2uvDALI3U5z05&index=1&ab_channel=Gronify
+#Gronify - Linear Regression Machine Learning Model in R | Practical Implementation of Linear regression model
+
 #1- load dataset
 
 data(longley)
@@ -16,3 +19,28 @@ ls(longley)
 
 ##summary of dataset
 summary(longley)
+
+#3 - Prepare dataset for Linear Regression Model
+
+##load appropriate library
+
+###caTools - https://cran.r-project.org/web/packages/caTools/index.html
+### install.packages("caTools")
+
+library('caTools')
+
+##generation random outputs
+set.seed(42)
+
+##split test and train data
+mySample = sample.split(longley, SplitRatio = .8)
+the_train = subset(longley, mySample==TRUE)
+head(the_train)
+str(the_train)
+the_test = subset(longley, mySample==FALSE)
+str(the_test)
+
+## the model will use the train  dataset ( GNP.deflator     GNP Unemployed Armed.Forces Population Year)
+## to predict number of people employed
+
+##4- Model Creation
