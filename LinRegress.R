0bd70ae2f878e7cc2a76e77d13691fc2b1604811
Megan Tran <68253811+Sonicdaheghod@users.noreply.github.com>
2023-08-24 20:38:51 -0400

    In Progress

---
+++ LinRegress.R
@@ -0,0 +1,18 @@
+#1- load dataset
+
+data(longley)
+
+#2 - Analyze dataset
+
+## print dimensions
+
+dim(longley)
+
+## print first few data points
+head(longley)
+
+## print out column names
+ls(longley)
+
+##summary of dataset
+summary(longley)
